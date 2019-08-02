bdc6301a19d857f8113b91d5a56db93a6e25950c
Lincoln Anderson <landerson@threefivetwo.com>
2019-08-01 21:56:13 -0400

    add auth user info

--- src/App.js
+++ src/App.js
@@ -6,15 +6,19 @@ function App() {
   const auth = useAuth();
 
   return (
-    <div>
+    <>
       <h1>HandyStandy</h1>
       {auth.isLoaded && auth.user && (
+        <>
+          <div>{auth.user.email}</div>
+          <img src={auth.user.photoURL} width={64} alt={auth.user.email}/>
           <button onClick={auth.signOut}>Sign Out</button>
-        )}
-      {auth.isLoaded && !auth.user && (
-          <button onClick={auth.signIn}>Sign in with Google</button>
+        </>
       )}
-    </div>
+      {auth.isLoaded && !auth.user && (
+        <button onClick={auth.signIn}>Sign in with Google</button>
+      )}
+    </>
   );
 }
 
